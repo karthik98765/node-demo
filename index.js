3a258103513cc94fc1c9ab3fb0923ad3f1199197
Rakshith G <rakshithgajendra@gmail.com>
2020-07-03 21:41:08 +0530

    Rakshith commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const chalk = require("chalk")
 
-console.log("This is karthik")
+console.log("This is Rakshih")
+console.log("This is Karthik")
 console.log(chalk.white.bgGreen('Hello world!'));
+
+console.log(chalk.white.bgGreen('Edited 1'));
+console.log(chalk.white.bgGreen('Edited 2'));
