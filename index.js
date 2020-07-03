e6a8cd434e87c1da519afef6f53f5f9733b79ad0
Rakshith G <rakshithgajendra@gmail.com>
2020-07-03 21:49:53 +0530

    init commit rakshith branch

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const chalk = require("chalk")
 
 console.log("This is Rakshih")
-console.log("This is Karthik")
-console.log(chalk.white.bgGreen('Hello world!'));
+console.log("This is Hemanth")
+console.log(chalk.white.bgGreen('Hello India!'));
 
-console.log(chalk.white.bgGreen('Edited 1'));
+console.log(chalk.white.bgGreen('Feature 1'));
 console.log(chalk.white.bgGreen('Edited 2'));
